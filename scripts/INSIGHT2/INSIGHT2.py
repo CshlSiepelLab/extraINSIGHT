cd52ffcc6538bd46c1fc5a31785be3fae9bb24e4
ndukler <ned36@cornell.edu>
2020-04-14 14:26:27 -0400

    Added INSIGHT2 pipeline

---
+++ scripts/INSIGHT2/INSIGHT2.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env python
+
+import argparse
+import os
+import shlex
+import shutil
+
+# Path to the INSIGHT package
+insight2_db = "/local/storage/data/extraINSIGHT/no-backup/data/INSIGHT2/Insight2DB"
+insight2 = "/sonas-hs/siepel/nlsas/data/projects/extraINSIGHT/bin/Insight2"
+
+# Set textwap width for parser
+os.environ['COLUMNS'] = "90"
+
+# Specify parser
+parser = argparse.ArgumentParser()
+parser.add_argument("-b","--bed", dest="bed", type=str,
+                    required=True, help="bed file containing query regions")
+parser.add_argument("-o","--out-dir", dest="out_dir", type=str,
+                    default = ".", required=True, help="results output directory (default = '.')")
+args = parser.parse_args()
+
+print(args)
+
+# Create output directory
+if not os.path.exists(args.out_dir):
+    os.makedirs(args.out_dir)
+    print("Directory " , args.out_dir ,  " Created ")
+else:    
+    print("Directory " , args.out_dir ,  " already exists")
+
+# Expand out paths
+args.bed = os.path.abspath(os.path.realpath(args.bed))
+args.out_dir = os.path.abspath(os.path.realpath(args.out_dir))
+
+# Get corename of bed file
+data_id = os.path.splitext(os.path.basename(args.bed))[0]
+
+# Copy the query bed file to the output directory
+shutil.copy(args.bed, args.out_dir)
+
+# remove bed suffix for insight2
+bed_string = os.path.join(args.out_dir, data_id)
+
+# Create command line call
+cmd = f"{insight2} {insight2_db} -fin {bed_string}"
+print(cmd)
+
+# Run INSIGHT2, it will automatically create output in the same directory as the bed file
+# which in this case is the output directory
+os.system(cmd)
---
+++ scripts/README.md
@@ -0,0 +1,8 @@
+The following is a description of the directories:
+    - analysis: code for all the analysis done for the extraINSIGHT paper
+    - config_files: configuration files for all pipelines
+    - download_data: contains the scripts to get gnomad data and other resources (chain file, etc.)
+    - extraINSIGHT: scripts and pipelines to compute fraction of sites under strong selection using extraINSIGHT
+    - genomic_covariates: scripts and pipelines to extract covariates used in mutation model for extraINSIGHT
+    - neutral_mutation_model: scripts and pipelines to fit neutral mutation model
+    - parse_gnomad: scripts and pipelines to parse gnomad data
